6a0817f916dd0188f58bfd3df55e096933cdfee5
Marcelo Lima <marcelo.lima@in.cassems.com.br>
2018-06-07 18:20:15 -0400

    implementacao tela de login e cadastro

--- app/src/main/java/br/ufms/facom/pegaaulaapp/LoginActivity.java
+++ app/src/main/java/br/ufms/facom/pegaaulaapp/LoginActivity.java
@@ -1,13 +1,41 @@
 package br.ufms.facom.pegaaulaapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
+
+
+    Button btnEntrar;
+    TextView tvRegistrar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        btnEntrar = findViewById(R.id.btn_entrar);
+        tvRegistrar = findViewById(R.id.lv_entrar);
+
+        tvRegistrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, CadastroActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnEntrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
